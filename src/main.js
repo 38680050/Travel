d98395ffeb5a2fdbfbbcea702e936960500cbe7f
chenhaoxuan <38680050@qq.com>
2020-07-03 23:05:08 +0800

    项目初始化配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// reset.css 重置浏览器样式
+// border.css 解决手机像素较高，可能会是二倍屏或三倍屏时，1px可能会根据css像素变成2px或3px的物理像素，引入可解决此问题
+// fastClick 移动端直接使用click事件时可能会有300毫秒延迟，引入可解决此问题
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
